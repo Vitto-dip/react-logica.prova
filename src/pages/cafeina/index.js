02a74d61c740b28db5d2988b559cb2f4fbb46016
Victor de Paula Freitas Simões <simoesvictor188@gmail.com>
2022-10-19 15:23:04 -0300

    calcular cafe interminado

---
+++ src/pages/cafeina/index.js
@@ -0,0 +1,38 @@
+import './index.scss'
+
+export default function Cafe() {
+    
+    function quantidadeCafe(N, L, D) {
+        
+        let alunos = 15;
+        let litros = 3;
+        let mililitros = 250;
+        let quantidadeCafelitros = (alunos * mililitros) / 1000;
+
+        if(quantidadeCafelitros > litros) {
+            let x = quantidadeCafelitros * 
+        }
+
+        console.log(quantidadeCafelitros)
+        
+        
+        
+        
+        
+        
+    }
+    
+    /*if(quantidadeCafelitros > litros) {
+            let x = Math.floor(quantidadeCafelitros)
+            valor = x * 2;
+            console.log(valor)
+        }
+        else {
+            console.log(quantidadeCafelitros)
+        }*/
+    return (
+        <main>
+
+        </main>
+    )
+}
